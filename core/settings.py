d241044ea63d2b9bf631f95c911848ebe4baa5d9
Ali Ahmad <turgut@Alis-MacBook-Pro.local>
2022-04-17 17:32:26 +0300

    semesters views + serializers | add it in api urls

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'djoser',
+    'semester'
 ]
 
 MIDDLEWARE = [
--- semester/views.py
+++ semester/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Semester
+from .serializers import SemesterViewSerializer
 # Create your views here.
+
+class SemesterViewSet(ModelViewSet):
+    queryset = Semester.objects.all()
+    serializer_class = SemesterViewSerializer
