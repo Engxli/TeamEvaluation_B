e29b9bac3f139e706e573dfbbfcfe1248199aaef
Ali Ahmad <turgut@Alis-MacBook-Pro.local>
2022-04-24 11:45:35 +0300

    cleaning output data

--- evaluation/models.py
+++ evaluation/models.py
@@ -6,3 +6,4 @@ import uuid
 class Evaluation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     project = models.OneToOneField(Project, on_delete=models.CASCADE, related_name="evaluation")
+    avg = models.JSONField(null=True, blank=True)
--- evaluation/serializers.py
+++ evaluation/serializers.py
@@ -1,57 +1,37 @@
-from pyexpat import model
-from django.forms import fields
 from rest_framework import serializers
 from .models import Evaluation
 from judge.serializers import JudgeSerializers
+
 class EvaluationSerializers(serializers.ModelSerializer):
     judge = JudgeSerializers(many=True, read_only=True)
-    # avg = serializers.SerializerMethodField(method_name="get_avg")
+    avg = serializers.SerializerMethodField(method_name="get_avg")
     class Meta:
         model = Evaluation
         fields = "__all__"
+
     def get_avg(self, obj:Evaluation):
         data = []
         objactData = {}
-
-        for judge in judge:
-            objactData['judge'] = {'id':judge['id'],"name":judge['name'], "grades":{}}
-            readGrade = {}
-            for teams in judge['grade']:
-                grades = [{grade['criteria_id']:grade['grade'] } for grade in teams['grade']]
-                print(grades)
-                for key in grades.key():
-                    readGrade[key]=[]
-                print(readGrade)
-
-                    
-
-
-                    # print(objactData["judge"]["grades"].keys())
-                    # print(grade['criteria_id'])
-                    # print(objactData["judge"]["grades"].keys())
-                    # objactData["judge"]["grades"][grade['criteria_id']] = 1
-                    # print(objactData["judge"]["grades"][grade['criteria_id']])
-                    # # objactData["judge"]["grades"][grade['criteria_id']] =  [grade['criteria_weight']]
-                    # if grade['criteria_id'] in objactData["judge"]["grades"].keys():
-                    #     print("true")
-                    #     objactData["judge"]["grades"][grade['criteria_id']].append(grade['criteria_weight'])
-                    # else:
-                    #     objactData["judge"]["grades"][grade['criteria_id']] =  [grade['criteria_weight']]
-                    #     print(objactData)
-
-
+        res = []
+        for marker in obj.judge.all():
+            for team in marker.grade:
+                for criteria in team['grade']:
+                    # print(criteria)
+                    if criteria['criteria_id'] in objactData.keys():
+                        objactData[criteria['criteria_id']]["grade"].append(criteria['grade'])
+                    else:
+                        objactData[criteria['criteria_id']] = {"judgeId": marker.id, "judgeName": marker.name}
+                        objactData[criteria['criteria_id']] = {**objactData[criteria['criteria_id']],"criteriaID": criteria['criteria_id']}
+                        objactData[criteria['criteria_id']] = {**objactData[criteria['criteria_id']], "criteriaName": criteria['criteria_name']}
+                        objactData[criteria['criteria_id']] = {**objactData[criteria['criteria_id']], "criteriaWeight": criteria['criteria_weight']}
+                        objactData[criteria['criteria_id']] = {**objactData[criteria['criteria_id']], "grade": [criteria['grade']]}
             data.append(objactData)
-                     # print(grade['criteria_id'])
-                    # objactData["judge"]["grades"].append({grade['criteria_id']:grade['criteria_weight']})
-
-
-
-        # data["total_judge"] = len(obj.judge.all())
-        # data['criteria'] = []
-
-        # for judge in obj.judge.all():
-        #     for teams in judge['grade'].all():
-        #         data['criteria'].append({judge['name']:[]})
-        #         for grade in teams['grade'].all():
-        #             data['criteria'][]
-        pass
+            objactData={}
+        
+        for marker in data:
+            for key,value in marker.items():
+                value['avgGrade'] = (sum(value['grade'])/len(value['grade']))*10
+                res.append(value)
+        
+        return res
+        
